59d20fd1906c09a9481b13f6c0840fb806e52564
Priyanka <55687162+priyanka24798@users.noreply.github.com>
2020-04-29 18:24:13 +0530

    Update application.py

--- project1/application.py
+++ project1/application.py
@@ -93,23 +93,84 @@ def authenticate():
     else:
         return render_template("registration.html", message = "Account does not exists..Please register!! ")
 
+
 @app.route('/Search', methods=["GET","POST"])
 def search():
     
     if(request.method == "POST"):
         book_search = request.form.get("search")
-
+        
         if request.form.get("isbnsearch") == "option1":
-            s = Books.query.filter(Books.isbn.like( book_search +'%')).all()
+            print(book_search +" like  "+"isbnsearch")
+            s = Books.query.filter(Books.isbn.like('%'+ book_search +'%')).all()
+            print(s)
             return render_template("user.html", Books = s)
 
         elif request.form.get("titlesearch") == "option2":
+            print(book_search +" like   "+"titlesearch")
             s = db1.session.query(Books).filter((Books.tittle.like('%'+ book_search +'%')))
+            print(s)
             return render_template("user.html", Books = s)
        
         elif request.form.get("authorsearch") == "option3":
+            print(book_search +" like   "+"authorsearch")
             s = db1.session.query(Books).filter((Books.author.like('%'+ book_search +'%')))
+            print(s)
             return render_template("user.html", Books = s)
         return render_template("user.html", message= "No books found.!")
     return render_template("user.html")
 
+
+@app.route("/bookpage/<string:isbn_id>")
+def book_details(isbn_id):
+    book = db1.session.query(Books).filter(Books.isbn == isbn_id).all()
+    review = db1.session.query(REVIEW).filter(REVIEW.isbn == isbn_id).all()
+    total_reviews = db.session.query(REVIEW).filter(REVIEW.isbn == isbn_id)
+    return render_template("bookpage.html", data=book, isbn_id = isbn_id, total_reviews = total_reviews)
+
+
+# @app.route('/review', methods =['GET','POST'])
+# def review():
+#     if request.method == 'POST':
+#         rating = request.form.get('review_tags')
+#         review = request.form.get('review_value')
+#         username = session['username']
+#         t= list(request.form.items())
+#         print(t)
+
+#         if (REVIEW.query.filter_by(email = email, isbn = "1234").first() == None) :
+#             data = REVIEW(email = email, isbn = "1234", rating = rating, review = review) 
+#             db1.session.add(data)
+#             db1.session.commit()
+#         else:
+#             total_reviews = db1.session.query(REVIEW).filter(REVIEW.isbn == "1234")
+#             return render_template('Review.html',message = 'You have already given review',total_reviews=total_reviews,isbn = "1234")
+#     total_reviews = db1.session.query(REVIEW).filter(REVIEW.isbn == "1234")
+#     return render_template('Review.html', message1 = 'review submitted succesfully.',total_reviews=total_reviews,isbn = "1234")
+
+
+
+
+@app.route('/review', methods =['GET','POST'])
+def review():
+    if request.method == 'POST':           
+        rating = request.form.get('review_tags')
+        review = request.form.get('review_value')
+        email = session['username']
+        temp = list(request.form.items())
+        print(temp)
+        isbn = temp[2][0]
+        book = db1.session.query(Books).filter(Books.isbn == isbn).all()
+        total_reviews = db.session.query(REVIEW).filter(REVIEW.isbn == isbn)
+        if (REVIEW.query.filter_by(email = email, isbn = isbn).first() == None):
+            data = REVIEW(email = email, isbn = isbn, rating = rating, review = review) 
+            db.session.add(data)
+            db.session.commit()
+        else:
+            return render_template('bookpage.html',message = 'You have already given review',data=book, total_reviews=total_reviews,isbn = isbn)
+    return render_template('bookpage.html',email = session['username'], message1 = 'review submitted succesfully.',data=book, total_reviews=total_reviews,isbn = isbn)
+
+@app.route("/logout")
+def logout(): 
+    session.pop('username', None)
+    return render_template("registration.html")
