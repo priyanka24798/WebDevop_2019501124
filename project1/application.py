02d6e77797d0f595c01f4481b7370fdf12192eb0
priyanka24798 <kotupriyanka@msitprogram.net>
2020-05-03 22:20:24 +0530

    integration

--- project1/application.py
+++ project1/application.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, session, request, url_for
 from flask import redirect
+from flask import jsonify, json
 from flask import Flask, render_template
 from flask_session import Session
 from sqlalchemy import create_engine
@@ -10,7 +11,8 @@ from database import *
 from datetime import datetime
 from booksimport import *
 from sqlalchemy import or_
-from bookreview import *
+from review import *
+import requests
 
 
 app = Flask(__name__)
@@ -91,20 +93,10 @@ def authenticate():
     else:
         return render_template("registration.html", message = "Account does not exists..Please register!! ")
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-@app.route("/bookpage/<string:isbn_id>")
-def book_details(isbn_id):
-    book = db1.session.query(Books).filter(Books.isbn == isbn_id).all()
-    return render_template("bookpage.html", data=book)
-=======
-=======
->>>>>>> master
-=======
 
->>>>>>> 59d20fd1906c09a9481b13f6c0840fb806e52564
+
+
 @app.route('/Search', methods=["GET","POST"])
 def search():
     
@@ -132,11 +124,9 @@ def search():
     return render_template("user.html")
 
 
-@app.route("/bookpage/<string: isbn_id>")
-def book_details(isbn_id):
-    book = db1.session.query(Books).filter(Books.isbn == isbn_id).all()
-    return render_template("bookpage.html", data = book)
-        
+
+
+
 
 
 @app.route("/bookpage/<string:isbn_id>")
@@ -145,26 +135,6 @@ def book_details(isbn_id):
     review = db1.session.query(REVIEW).filter(REVIEW.isbn == isbn_id).all()
     total_reviews = db.session.query(REVIEW).filter(REVIEW.isbn == isbn_id)
     return render_template("bookpage.html", data=book, isbn_id = isbn_id, total_reviews = total_reviews)
-
-
-# @app.route('/review', methods =['GET','POST'])
-# def review():
-#     if request.method == 'POST':
-#         rating = request.form.get('review_tags')
-#         review = request.form.get('review_value')
-#         username = session['username']
-#         t= list(request.form.items())
-#         print(t)
-
-#         if (REVIEW.query.filter_by(email = email, isbn = "1234").first() == None) :
-#             data = REVIEW(email = email, isbn = "1234", rating = rating, review = review) 
-#             db1.session.add(data)
-#             db1.session.commit()
-#         else:
-#             total_reviews = db1.session.query(REVIEW).filter(REVIEW.isbn == "1234")
-#             return render_template('Review.html',message = 'You have already given review',total_reviews=total_reviews,isbn = "1234")
-#     total_reviews = db1.session.query(REVIEW).filter(REVIEW.isbn == "1234")
-#     return render_template('Review.html', message1 = 'review submitted succesfully.',total_reviews=total_reviews,isbn = "1234")
 
 
 
@@ -187,30 +157,10 @@ def review():
         else:
             return render_template('bookpage.html',message = 'You have already given review',data=book, total_reviews=total_reviews,isbn = isbn)
     return render_template('bookpage.html',email = session['username'], message1 = 'review submitted succesfully.',data=book, total_reviews=total_reviews,isbn = isbn)
->>>>>>> 59d20fd1906c09a9481b13f6c0840fb806e52564
 
 @app.route("/logout")
 def logout(): 
     session.pop('username', None)
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-
-# @app.route("/bookpage")
-# def books():
-#     # ISBN = Books.query.get("isbn")
-#     isbn = "1416949658"
-#     details =  Books.query.filter_by(isbn = isbn)
-#     # review = "SELECT * FROM REVIEW WHERE isbn = 1234"
-#     return render_template("bookpage.html",data=details)
-
-
-
-
-
-   
-=======
->>>>>>> 5acbc0c81aac67cf749de6a66ac39481a297ae64
-=======
     return render_template("registration.html")
->>>>>>> master
+
+    
